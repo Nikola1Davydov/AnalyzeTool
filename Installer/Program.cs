150476aea964f23cbbafbb0a5ffe8ac7f553b6a5
Nikolai Davydov <nik_davydov_1992@outlook.com>
2025-06-08 19:12:13 +0200

    установочник готов к использованию

--- Installer/Program.cs
+++ Installer/Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using WixSharp;
-using WixSharp.UI.WPF;
 
 namespace Installer
 {
@@ -11,25 +10,48 @@ namespace Installer
         private static void Main()
         {
             System.IO.DirectoryInfo solutionPath = TryGetSolutionDirectoryInfo();
-            string plaginPath24 = Path.Combine(solutionPath.FullName, @"AnalyseTool\bin\Release R24\publish\Revit 2024 Release R24 addin", @"*.*");
-            string targetPath24 = Path.Combine("%CommonAppDataFolder%", @"Autodesk\Revit\Addins\2024");
-            Compiler.GetMappedWixConstants(true).ForEach(Console.WriteLine);
+            string targetPath(string revitVersion) => Path.Combine("%CommonAppDataFolder%", $@"Autodesk\Revit\Addins\20{revitVersion}");
+            string plaginPath(string revitVersion) => Path.Combine(solutionPath.FullName, $@"AnalyseTool\bin\Release R{revitVersion}\publish\Revit 20{revitVersion} Release R{revitVersion} addin", @"*.*");
+
+            string plaginPath23 = plaginPath("23");
+            string plaginPath24 = plaginPath("24");
+            string plaginPath25 = plaginPath("25");
+
+            string targetPath23 = targetPath("23");
+            string targetPath24 = targetPath("24");
+            string targetPath25 = targetPath("25");
+
+            string licencePath = Path.Combine(solutionPath.FullName, "LICENSE.rtf");
 
             Project project = new Project("AnalyzeTool",
-                              new Dir(targetPath24,
-                                  new Files(plaginPath24)));
+                              new Dir(targetPath23,
+                                  new Files(plaginPath23)),
+                                    new Dir(targetPath24,
+                                  new Files(plaginPath24)),
+                                    new Dir(targetPath25,
+                                  new Files(plaginPath25)));
 
-            //  todo: add icon
             project.GUID = new Guid("e74a2dbc-8131-4240-abde-e2a776451bba");
             project.Version = new Version(1, 0, 1, 0);
             project.Scope = InstallScope.perUser;
-            project.LicenceFile = "";
-            project.UI = WUI.WixUI_ProgressOnly;
+            project.LicenceFile = licencePath;
+            project.UI = WUI.WixUI_InstallDir;
+
+            project.BackgroundImage = ""; // 493 x 312 pixels
+            project.BannerImage = ""; // 493 x 58 pixels
+            project.ControlPanelInfo = new ProductInfo
+            {
+                Manufacturer = "Nikolai Davydov",
+                ProductIcon = Path.Combine(solutionPath.FullName, @"SharedData\Resources\Icons\AnalyzeTool_Icon.ico"),
+                Name = "AnalyseTool",
+                Readme = "https://github.com/Nikola1Davydov/AnalyzeTool",
+                HelpLink = "https://github.com/Nikola1Davydov/AnalyzeTool",
+                UrlInfoAbout = "https://github.com/Nikola1Davydov/AnalyzeTool",
+                UrlUpdateInfo = "https://github.com/Nikola1Davydov/AnalyzeTool/releases"
+            };
 
             project.BuildMsi();
         }
-
-        // todo: add check for solution path
         public static System.IO.DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
         {
             System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(
