6e6ac965404c2ee1a74b66300eb81dc93a9ed8d8
Nikolai Davydov <nik_davydov_1992@outlook.com>
2025-09-17 21:59:20 +0200

    create assemblyInfo and file for constant data

---
+++ AnalyseTool/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using SharedData;
+using System.Reflection;
+
+// Общая информация о сборке
+[assembly: AssemblyTitle(ToolData.PLUGIN_NAME)]
+[assembly: AssemblyProduct(ToolData.PLUGIN_NAME)]
+
+// Версия сборки
+// Формат: [Major].[Minor].[Build].[Revision]
+[assembly: AssemblyVersion(ToolData.PLUGIN_VERSION)]        // используется CLR для связывания
+[assembly: AssemblyFileVersion(ToolData.PLUGIN_VERSION)]   // видна в свойствах файла (Windows Explorer)
+[assembly: AssemblyInformationalVersion(ToolData.PLUGIN_VERSION)] // можно указывать "1.0.0-beta"
---
+++ SharedData/ToolData.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedData
+{
+    public static class ToolData
+    {
+        public const string PLUGIN_NAME = "Analyse Tool";
+        public const string PLUGIN_VERSION = "2.0.0.0";
+    }
+}
