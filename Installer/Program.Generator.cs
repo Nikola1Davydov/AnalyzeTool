ac4b2275d20d54b652ed09c4ce64b12b17c9430d
Nikolai Davydov <nik_davydov_1992@outlook.com>
2025-09-19 16:52:35 +0200

    finish with installer

---
+++ Installer/Program.Generator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using WixSharp;
+
+namespace Installer;
+
+public static class Generator
+{
+    /// <summary>
+    ///     Generates Wix entities, features and directories for the installer.
+    /// </summary>
+    public static WixEntity[] GenerateWixEntities(IEnumerable<string> args)
+    {
+        Regex versionRegex = new Regex(@"\d+");
+        Dictionary<string, List<WixEntity>> versionStorages = new Dictionary<string, List<WixEntity>>();
+
+        Feature revitFeature = new Feature
+        {
+            Name = "Revit Add-in",
+            Description = "Revit add-in installation files",
+            Display = FeatureDisplay.expand
+        };
+
+        foreach (string directory in args)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+            string fileVersionNumber = versionRegex.Match(directoryInfo.Parent.Name).Value;
+            string fileVersion = (int.Parse(fileVersionNumber) + 2000).ToString();
+            Feature feature = new Feature
+            {
+                Name = fileVersion,
+                Description = $"Install add-in for Revit {fileVersion}",
+                ConfigurableDir = $"INSTALL{fileVersion}"
+            };
+
+            revitFeature.Add(feature);
+
+            Files files = new Files(feature, $@"{directory}\*.*", FilterEntities);
+            if (versionStorages.TryGetValue(fileVersion, out List<WixEntity>? storage))
+            {
+                storage.Add(files);
+            }
+            else
+            {
+                versionStorages.Add(fileVersion, [files]);
+            }
+
+            LogFeatureFiles(directory, fileVersion);
+        }
+
+        return versionStorages
+            .Select(storage => new Dir(new Id($"INSTALL{storage.Key}"), storage.Key, storage.Value.ToArray()))
+            .Cast<WixEntity>()
+            .ToArray();
+    }
+
+    /// <summary>
+    ///     Filter installer files and exclude from output. 
+    /// </summary>
+    private static bool FilterEntities(string file)
+    {
+        return !file.EndsWith(".pdb");
+    }
+
+    /// <summary>
+    ///    Write a list of installer files.
+    /// </summary>
+    private static void LogFeatureFiles(string directory, string fileVersion)
+    {
+        string[] assemblies = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+        Console.WriteLine($"Installer files for version '{fileVersion}':");
+
+        foreach (string? assembly in assemblies.Where(FilterEntities))
+        {
+            Console.WriteLine($"'{assembly}'");
+        }
+    }
+}
--- Installer/Program.cs
+++ Installer/Program.cs
@@ -1,76 +1,94 @@
-﻿using System;
+﻿using Installer;
+using System;
 using System.IO;
 using System.Linq;
 using WixSharp;
 
-namespace Installer
+const string outputName = SharedData.ToolData.PLUGIN_NAME;
+const string projectName = SharedData.ToolData.PLUGIN_NAME;
+const string projectVersion = SharedData.ToolData.PLUGIN_VERSION;
+
+System.IO.DirectoryInfo solutionPath = TryGetSolutionDirectoryInfo();
+string targetPath(string revitVersion) => Path.Combine("%CommonAppDataFolder%", $@"Autodesk\Revit\Addins\20{revitVersion}");
+string plaginPath(string revitVersion)
 {
-    public class Program
+    int revitVersionInt = int.Parse(revitVersion);
+    if (revitVersionInt < 25)
     {
-        private static void Main()
-        {
-            System.IO.DirectoryInfo solutionPath = TryGetSolutionDirectoryInfo();
-            string targetPath(string revitVersion) => Path.Combine("%CommonAppDataFolder%", $@"Autodesk\Revit\Addins\20{revitVersion}");
-            string plaginPath(string revitVersion)
-            {
-                int revitVersionInt = int.Parse(revitVersion);
-                if (revitVersionInt < 25)
-                {
-                    return Path.Combine(solutionPath.FullName, $@"AnalyseTool\bin\Release R{revitVersion}\net48", @"*.*");
-                }
-                {
-                    return Path.Combine(solutionPath.FullName, $@"AnalyseTool\bin\Release R{revitVersion}\net8.0-windows", @"*.*");
-                }
-            }
-
-            string plaginPath24 = plaginPath("24");
-            string plaginPath25 = plaginPath("25");
-            string plaginPath26 = plaginPath("26");
-
-            string targetPath24 = targetPath("24");
-            string targetPath25 = targetPath("25");
-            string targetPath26 = targetPath("26");
-
-            string licencePath = Path.Combine(solutionPath.FullName, "LICENSE.rtf");
-
-            Project project = new Project(SharedData.ToolData.PLUGIN_NAME,
-                          new Dir(new Id("Revit2024Addins"), targetPath24,
-                                    new Files(plaginPath24)),
-                                new Dir(new Id("Revit2025Addins"), targetPath25,
-                                    new Files(plaginPath25)),
-                                new Dir(new Id("Revit2026Addins"), targetPath26,
-                                    new Files(plaginPath26))
-                                    );
-
-            project.GUID = new Guid("e74a2dbc-8131-4240-abde-e2a776451bba");
-            project.Version = new Version(SharedData.ToolData.PLUGIN_VERSION);
-            project.LicenceFile = licencePath;
-            project.UI = WUI.WixUI_InstallDir;
-
-            project.BackgroundImage = ""; // 493 x 312 pixels
-            project.BannerImage = ""; // 493 x 58 pixels
-            project.ControlPanelInfo = new ProductInfo
-            {
-                Manufacturer = SharedData.ToolData.PLUGIN_AUTHOR,
-                ProductIcon = Path.Combine(solutionPath.FullName, "SharedData/Resources/Icons/AnalyzeTool_Icon.ico"),
-                Name = SharedData.ToolData.PLUGIN_NAME,
-                Readme = SharedData.ToolData.LINK_TO_GITHUB,
-                HelpLink = SharedData.ToolData.LINK_TO_GITHUB,
-                UrlInfoAbout = SharedData.ToolData.LINK_TO_GITHUB,
-                UrlUpdateInfo = SharedData.ToolData.LINK_TO_DOWNLOADS
-            };
-
-            project.BuildMsi();
-        }
-        public static System.IO.DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
-        {
-            System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(
-                currentPath ?? System.IO.Directory.GetCurrentDirectory());
-            while (directory != null && !directory.GetFiles("*.sln").Any())
-            {
-                directory = directory.Parent;
-            }
-            return directory;
-        }
+        return Path.Combine(solutionPath.FullName, $@"AnalyseTool\bin\Release R{revitVersion}\net48");
     }
+    {
+        return Path.Combine(solutionPath.FullName, $@"AnalyseTool\bin\Release R{revitVersion}\net8.0-windows");
+    }
+}
+
+string[] versions =
+{
+    plaginPath("24"),
+    plaginPath("25"),
+    plaginPath("26"),
+};
+
+
+Project project = new Project
+{
+    OutDir = "output",
+    Name = projectName,
+    Platform = Platform.x64,
+    UI = WUI.WixUI_InstallDir,
+    MajorUpgrade = MajorUpgrade.Default,
+    GUID = new Guid("e74a2dbc-8131-4240-abde-e2a776451bba"),
+    LicenceFile= Path.Combine(solutionPath.FullName, "Installer", "LICENSE.rtf"),
+    //BannerImage = @"",
+    //BackgroundImage = @"",
+    Version = new Version(projectVersion),
+    ControlPanelInfo =
+    {
+        Manufacturer = SharedData.ToolData.PLUGIN_AUTHOR,
+        //ProductIcon = Path.Combine(solutionPath.FullName, "SharedData/Resources/Icons/AnalyzeTool_Icon.ico"),
+        Name = SharedData.ToolData.PLUGIN_NAME,
+        Readme = SharedData.ToolData.LINK_TO_GITHUB,
+        HelpLink = SharedData.ToolData.LINK_TO_GITHUB,
+        UrlInfoAbout = SharedData.ToolData.LINK_TO_GITHUB,
+        UrlUpdateInfo = SharedData.ToolData.LINK_TO_DOWNLOADS
+    }
+};
+
+WixEntity[] wixEntities = Generator.GenerateWixEntities(versions);
+//project.RemoveDialogsBetween(NativeDialogs.WelcomeDlg, NativeDialogs.CustomizeDlg);
+
+BuildSingleUserMsi();
+BuildMultiUserUserMsi();
+
+void BuildSingleUserMsi()
+{
+    project.InstallScope = InstallScope.perUser;
+    project.OutFileName = $"{outputName}-{project.Version}-SingleUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%AppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
+
+void BuildMultiUserUserMsi()
+{
+    project.InstallScope = InstallScope.perMachine;
+    project.OutFileName = $"{outputName}-{project.Version}-MultiUser";
+    project.Dirs =
+    [
+        new InstallDir(@"%CommonAppDataFolder%\Autodesk\Revit\Addins\", wixEntities)
+    ];
+    project.BuildMsi();
+}
+
+static System.IO.DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
+{
+    System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(
+        currentPath ?? System.IO.Directory.GetCurrentDirectory());
+    while (directory != null && !directory.GetFiles("*.sln").Any())
+    {
+        directory = directory.Parent;
+    }
+    return directory;
 }
